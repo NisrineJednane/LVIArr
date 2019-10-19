b573aa453027abe4497e563eabbd450febd99738
NisrineJednane <34342007+NisrineJednane@users.noreply.github.com>
2019-10-19 17:37:36 +0100

    Update lviar_algo.js

--- lviar_algo.js
+++ lviar_algo.js
@@ -1,8 +1,8 @@
 //algorithm using map and reduce 
 
 function largestValueinArray(xarr) {
-    return xarr.map(function(narr) {
-      return narr.reduce(function(a,b) {
+    return xarr.map((narr) => {
+      return narr.reduce((a,b) => {
         return (a > b) ? a : b;
       })
     })
